d7da6afa38129dfd1c9af625639b7ae8ba0d0a56
Ben Potter <epiclyepicisepic@gmail.com>
2018-05-16 09:38:57 -0400

    Started item classes

--- ShoppersDrugMartBackRoomProgramV0.01/ShoppersDrugMartBackRoomProgramV0.01/Items.h
+++ ShoppersDrugMartBackRoomProgramV0.01/ShoppersDrugMartBackRoomProgramV0.01/Items.h
@@ -1,1 +1,34 @@
+#include <iostream>
 #include <vector>
+using namespace std;
+
+#define FilePrefix "/"
+#define FileSuffix "/items.dat"
+
+class Item {
+
+	int upc, plu, amount;
+	char name[15], desc[25];
+	float price, cost, sale;
+
+
+};
+
+class Database {
+
+public:
+	Database();
+	~Database();
+	void Add(int upc, int plu, int amount, char name[15], char desc[25], float price, float cost, float sale);
+
+
+private:
+	std::vector<Item> item(1);
+
+};
+
+Database::Database() {
+
+
+
+}
