9a4362481bc31d665650399c0cd5ce76822a788c
Mark M <boxboy12345@hotmail.com>
2018-05-16 08:57:14 -0400

    Started logger

--- ShoppersDrugMartBackRoomProgramV0.01/ShoppersDrugMartBackRoomProgramV0.01/Logger.h
+++ ShoppersDrugMartBackRoomProgramV0.01/ShoppersDrugMartBackRoomProgramV0.01/Logger.h
@@ -1,1 +1,29 @@
-#include < dfg
+#include <conio.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+#define FilePrefix "/"
+#define FileSuffix "/logger.log"
+
+class Logger
+{
+public:
+	Logger();
+	~Logger();
+
+private:
+
+	string Filepath;
+
+};
+
+Logger::Logger()
+{
+
+
+}
+
+Logger::~Logger()
+{
+}
