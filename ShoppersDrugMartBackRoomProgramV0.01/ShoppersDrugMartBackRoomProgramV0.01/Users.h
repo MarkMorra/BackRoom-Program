fbd933960dd455adb23e305a7eb876fd944bb7cf
Michiko <theidiotprodigy@gmail.com>
2018-05-17 10:29:39 -0400

    Started on User Database ... I have no idea what I'm doing

--- ShoppersDrugMartBackRoomProgramV0.01/ShoppersDrugMartBackRoomProgramV0.01/Users.h
+++ ShoppersDrugMartBackRoomProgramV0.01/ShoppersDrugMartBackRoomProgramV0.01/Users.h
@@ -1,1 +1,33 @@
-#pragma once
+//Thursday, May 17, 2018
+
+#include <conio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <vector>
+
+using namespace std;
+
+#define SIZE 100
+
+class Permissions //Contains permissions for the specific username or password, accessed through Database class
+{
+public:
+	bool test; //Cady doesn't know how to do this
+};
+
+class Users //Contains usernames and passwords, accessed through Database class
+{
+public:
+};
+
+class Database
+{
+public:
+	vector<Users> user;
+	vector<Permissions> permission;
+
+	//Username, password, permissions, access to these is here
+};
