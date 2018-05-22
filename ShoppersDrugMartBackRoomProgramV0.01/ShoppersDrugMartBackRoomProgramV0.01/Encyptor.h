02c07bc5c5e9b321d0989b0f483850442dcda50d
Mark M <boxboy12345@hotmail.com>
2018-05-22 09:10:11 -0400

    encryptor is working

--- ShoppersDrugMartBackRoomProgramV0.01/ShoppersDrugMartBackRoomProgramV0.01/Encyptor.h
+++ ShoppersDrugMartBackRoomProgramV0.01/ShoppersDrugMartBackRoomProgramV0.01/Encyptor.h
@@ -20,13 +20,33 @@ void encrypt(char *string)
 			*(string + (sizeof(char))*pos) += previousValue;
 			previousValue = temp;
 		}
-
+		pos++;
 	}
 }
 
 void encrypt(char *string,int length)
 {
 
+	int pos = 0;
+	char previousValue, temp;
+
+	while (*(string + (sizeof(char))*pos) != '\0')
+	{
+
+		if (pos == 0)
+		{
+			previousValue = *string;
+			*string += 10;
+		}
+		else
+		{
+			temp = *(string + (sizeof(char))*pos);
+			*(string + (sizeof(char))*pos) += previousValue;
+			previousValue = temp;
+		}
+		pos++;
+		if (pos >= length) { return; }
+	}
 
 
 }
@@ -34,7 +54,42 @@ void encrypt(char *string,int length)
 void decrypt(char *string) 
 {
 
+	int pos = 0;
 
+	while (*(string + (sizeof(char))*pos) != '\0')
+	{
 
+		if (pos == 0)
+		{
+			*string -= 10;
+		}
+		else
+		{
+			*(string + (sizeof(char))*pos) -= *(string + (sizeof(char))*(pos-1));
+		}
+		pos++;
+	}
+
+}
+
+void decrypt(char *string, int length)
+{
+	int pos = 0;
+
+	while (*(string + (sizeof(char))*pos) != '\0')
+	{
+
+		if (pos == 0)
+		{
+			*string -= 10;
+		}
+		else
+		{
+			*(string + (sizeof(char))*pos) -= *(string + (sizeof(char))*(pos - 1));
+		}
+		pos++;
+		if (pos >= length) { return; }
+
+	}
 }
 
