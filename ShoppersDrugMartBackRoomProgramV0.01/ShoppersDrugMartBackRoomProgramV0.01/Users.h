10cea8556edbde09a7fee605598527c52f8e1e16
Mark M <boxboy12345@hotmail.com>
2018-05-22 13:21:43 -0400

    worked on user

--- ShoppersDrugMartBackRoomProgramV0.01/ShoppersDrugMartBackRoomProgramV0.01/Users.h
+++ ShoppersDrugMartBackRoomProgramV0.01/ShoppersDrugMartBackRoomProgramV0.01/Users.h
@@ -6,6 +6,8 @@
 #include "Encryptor.h"
 using namespace std;
 
+#define MAX_USERS 2000
+
 #define P_SELECTITEM 0
 #define P_RESETITEM 1
 #define P_VIEWLOGS 2
@@ -37,11 +39,7 @@ Permissions::Permissions()
 	}
 }
 
-inline Permissions::Permissions(bool perms[NUMBER_OF_PERMISSIONS])
-{
-}
-
-Permissions::Permissions(bool perms[])
+Permissions::Permissions(bool perms[NUMBER_OF_PERMISSIONS])
 {
 	for (int i = 0; i < NUMBER_OF_PERMISSIONS; i++)
 	{
@@ -63,6 +61,13 @@ public:
 	
 };
 
+User::User()
+{
+	id = 0;
+	strcpy(firstName, "");
+	strcpy(lastName, "");
+}
+
 User::User(int _ID, string _firstName, string _lastName) //Variables from main send information here
 {
 	id = _ID;
@@ -75,11 +80,28 @@ class UserDatabase
 public:
 	UserDatabase();
 	~UserDatabase();
+
+	void Add(User user);
 private:
 	vector<User> users;
 
 	int findWithID(int ID);
 };
+
+UserDatabase::Add(User user)
+{
+	do
+	{
+		user.id = rand() % MAX_USERS;
+	} while (findWithID(user.id >= 0)); //checks if the id has already been used
+
+	//bens code to find the position it should be added based on its ID
+
+	//users.insert(); ---actaully add id
+	
+
+
+}
 
 int UserDatabase::findWithID(int ID)
 {
